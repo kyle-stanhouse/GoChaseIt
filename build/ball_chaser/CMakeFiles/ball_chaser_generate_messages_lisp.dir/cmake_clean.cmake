940fe4e80cfa0d2835c8b6d2b3c5cd64b7e63fbe
Kyle Stanhouse <kstanhouse@gmail.com>
2020-11-16 21:01:22 -0800

    removed build and devel folders

--- build/ball_chaser/CMakeFiles/ball_chaser_generate_messages_lisp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/parallels/workspace/catkin_ws/devel/share/common-lisp/ros/ball_chaser/srv/DriveToTarget.lisp"
-  "CMakeFiles/ball_chaser_generate_messages_lisp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ball_chaser_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
