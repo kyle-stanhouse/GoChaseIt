245c4639a8d282e7d32dee5769bd98a9077826c9
Kyle Stanhouse <kstanhouse@gmail.com>
2020-11-17 17:30:03 -0800

    fixed my robot urdf to execute correct turns, fixes to process_image

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -26,6 +26,8 @@ void process_image_callback(const sensor_msgs::Image img)
 {
 
     int white_pixel = 255;
+    int white_pixel_idx;
+    bool found_white_ball = false;	 
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
@@ -34,35 +36,47 @@ void process_image_callback(const sensor_msgs::Image img)
 
     for (int i = 0; i < img.height * img.step; i++) {
 
-        ROS_INFO_STREAM("Got inside loop, img.data[i]" << std::to_string(img.data[i]));
+        ROS_INFO_STREAM("img.data[i]" << std::to_string(img.data[i]));
 
 	// find white pixel
         if (img.data[i] == white_pixel) {
-
-		ROS_INFO_STREAM("Got inside if");
-    
-	    // Decide to drive robot left, forward, right, or stop
-            if ( (i % img.width) < fabs(img.step/3) ){
-		drive_robot(0.0,0.5); //drive left
-	    } 
-	    else if( (i % img.width) >= fabs(img.step/3) & i < fabs(img.step * 2/3) ){
-	    	drive_robot(0.5,0.0); //drive forward
-	    } 
-	    else if( (i % img.width) >= fabs(img.step*2/3) ){
-		drive_robot(0.0,-0.5); //drive right
-	    } 
-	    else{
-		ROS_ERROR("Couldn't find drive direction");
-	    }
-
-        } // end if
-	else{
-
-	    drive_robot(0.0,0.0); //stop robot
-
-	} // end if/else
- ROS_INFO_STREAM("GOT THROUGH LOOP");
+	    ROS_INFO_STREAM("Found white ball!");
+	    white_pixel_idx = i;
+	    found_white_ball = true;
+	    break;
+	}
     } //end for
+    
+    if (found_white_ball){
+
+	// Decide to drive robot left, forward, right, or stop
+        if ( (white_pixel_idx % img.width) < fabs(img.step/3) ){
+		ROS_INFO_STREAM("Driving left");		
+		drive_robot(0.0,0.5); //drive left
+	} 
+	else if( (white_pixel_idx % img.width) >= fabs(img.step/3) & white_pixel_idx < fabs(img.step * 2/3) ){
+		ROS_INFO_STREAM("Driving forward");	    	
+		drive_robot(0.5,0.0); //drive forward
+	} 
+	else if( (white_pixel_idx % img.width) >= fabs(img.step*2/3) ){
+		ROS_INFO_STREAM("Driving right");		
+		drive_robot(0.0,-0.5); //drive right
+	} 
+	else{
+	     ROS_ERROR("Couldn't find drive direction");
+	     drive_robot(0.0,0.0); //stop robot 
+	}
+
+	// reset flag
+	found_white_ball = false;
+
+    } //end if
+    else{
+	  ROS_INFO_STREAM("Couldn't find white ball!");
+	} 	
+
+    	ROS_INFO_STREAM("Reached end of callback");	
+
 } // end process_image_callback
 
 int main(int argc, char** argv)
